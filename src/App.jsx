719e28cbdd3420f5a2eac210ce1e375a06c9bdb1
Sidhjain17 <sidhjain.17@gmail.com>
2025-12-01 16:55:34 +0530

    useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import { Route, Routes } from 'react-router-dom';
